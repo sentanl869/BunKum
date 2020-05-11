748acd0390f3dda62a9afef03d655c146d93e487
sentanl869 <sentanl869@gmail.com>
2020-05-11 19:14:47 +0800

    Improve BaseModel

--- models/__init__.py
+++ models/__init__.py
@@ -10,3 +10,36 @@ class BaseModel:
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     created_time = Column(Integer, default=int(time()))
     updated_time = Column(Integer, default=int(time()))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def new(cls, form: dict):
+        m = cls()
+        for name, value in form.items():
+            setattr(m, name, value)
+        m.save()
+        return m
+
+    @classmethod
+    def update(cls, _id, **kwargs):
+        m = cls.query.filter_by(id=_id).first()
+        for name, value in kwargs.items():
+            setattr(m, name, value)
+        m.save()
+
+    @classmethod
+    def all(cls, **kwargs):
+        ms = cls.query.filter_by(**kwargs).all()
+        return ms
+
+    @classmethod
+    def one(cls, **kwargs):
+        ms = cls.query.filter_by(**kwargs).first()
+        return ms
+
+    @classmethod
+    def columns(cls):
+        return cls.__mapper__.c.items()
