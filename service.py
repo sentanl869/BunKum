ebf6dc400642396a6a94894a4e635be352417944
sentanl869 <sentanl869@gmail.com>
2021-01-11 17:37:34 +0800

    Improve service.py

--- service.py
+++ service.py
@@ -3,6 +3,9 @@ import os
 from app import configured_app
 from models.extensions import db
 from models.user import User
+from models.blog import Blog
+from models.comment import Comment
+from models.message import Message
 from models.category import Category
 from models.role import Role, Permission
 
@@ -15,7 +18,11 @@ def make_shell_context() -> dict:
     return {
         'db': db,
         'User': User,
+        'Blog': Blog,
         'Role': Role,
+        'Comment': Comment,
+        'Message': Message,
+        'Category': Category,
         'Permission': Permission,
     }
 
