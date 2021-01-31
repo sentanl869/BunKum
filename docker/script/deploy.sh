fa3044040576ece478f8caf2f46235bc6b62f462
sentanl869 <sentanl869@gmail.com>
2021-02-01 03:08:50 +0800

    Fix deploy.sh connection test bug.

--- docker/script/deploy.sh
+++ docker/script/deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-until nc -nz mysql 3306 ; do
-  >&2 echo "[ *** Notice *** ]: mysql is unavailable - retrying"
+until nc -vz mysql 3306 ; do
   sleep 3
 done
 
 cd /var/www/BunKum
 
 python3 reset_app.py
+
 export FLASK_APP=service.py
+
 flask deploy
