06cde1166a63e076076907a376ec6dad33cdcb1e
sentanl869 <sentanl869@gmail.com>
2021-04-12 11:04:30 +0800

    Improve JavaScript file syntax.

--- static/script.js
+++ static/script.js
@@ -1,30 +1,30 @@
-let changeTableClassName = function () {
-    let tables = document.getElementsByTagName("table");
+const changeTableClassName = () => {
+    let tables = document.getElementsByTagName("table")
     for (let i=0; i<tables.length; i++) {
         tables[i].className = "table table-bordered table-striped"
     }
-};
-
-let getCommentAuthorName = function () {
-    let commentAuthors = document.querySelectorAll(".comment-author");
-    let authorSet = new Set();
-    for (let i=0; i<commentAuthors.length; i++) {
-        authorSet.add(commentAuthors[i].innerText);
-    }
-    let authorList = Array.from(authorSet);
-    let result = [];
-    for (let i=0; i<authorList.length; i++) {
-        let value = {
-            key: authorList[i],
-            value: authorList[i],
-        };
-        result.push(value);
-    }
-    return result;
-};
-
-let __main = function () {
-    changeTableClassName();
 }
 
-__main();
+const getCommentAuthorName = () => {
+    let commentAuthors = document.querySelectorAll(".comment-author")
+    let authorSet = new Set()
+    for (let i=0; i<commentAuthors.length; i++) {
+        authorSet.add(commentAuthors[i].innerText)
+    }
+    let authorList = Array.from(authorSet)
+    let result = []
+    authorList.forEach(author => {
+        let value = {
+            key: author,
+            value: author,
+        }
+        result.push(value)
+    })
+    return result
+}
+
+const __main = () => {
+    changeTableClassName()
+}
+
+__main()
